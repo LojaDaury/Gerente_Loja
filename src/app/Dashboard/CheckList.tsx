a898c8836fc4ecd144bb66849f504a069f457ac9
unknown <lojadaury@gmail.com>
2024-05-04 16:24:00 -0400

    photo_tela_interface

--- src/app/Dashboard/CheckList.tsx
+++ src/app/Dashboard/CheckList.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { IoPersonCircle, IoTrash } from "react-icons/io5";
+import { IoPersonCircle, IoTrash, IoCheckmark  } from "react-icons/io5";
 import { IoIosClose } from "react-icons/io";
 
 import Image from "next/image";
@@ -17,7 +17,7 @@ export default function CheckList() {
 
     const getVideo = () => {
 
-        console.log(innerWidth, innerHeight)
+        setHasPhoto(false)
         
         navigator.mediaDevices
         .getUserMedia({
@@ -87,12 +87,19 @@ export default function CheckList() {
                         <canvas ref={photoRef} 
                             className={`g-slate-400 absolute top-0 right-0 bottom-0 duration-500  ${ hasPhoto ? 'left-0': '-left-[1999px]'}`}/>
                         
-                        <button onClick={() => {setCam(false), setHasPhoto(false)}} 
+                        <button onClick={() => {setCam(false)}} 
                             className="absolute top-6 left-6 bg-white text-black rounded-full duration-300 hover:top-5 hover:left-7">
                             <IoIosClose size={40}/>        
                         </button>
                         
-                        <div className="flex justify-center items-end gap-6 absolute bottom-12 left-2/4">
+                        <div className="flex justify-center items-end gap-6 absolute bottom-12 left-0 right-0">
+
+                            <button onClick={() => setCam(false)} 
+                                className={`bg-white hover:bg-gray-400 duration-500 
+                                    border-2 border-white p-2 
+                                    rounded-full ${hasPhoto?'bg-green-400 text-white hover:bg-green-600': 'bg-white text-black'}`}>
+                                <IoCheckmark  size={28}/>
+                            </button>
 
                             <button onClick={takePhoto} 
                                 className="bg-gray-600 hover:bg-gray-300 duration-500 
@@ -100,8 +107,8 @@ export default function CheckList() {
                                     rounded-full"/>
 
                             <button onClick={() => setHasPhoto(false)} 
-                                className="bg-white hover:bg-gray-200 duration-500 
-                                    border-2 border-gray-400 hover:border-gray-600 p-2 
+                                className="bg-white hover:bg-gray-400 duration-500 
+                                    border-2 border-white hover:border-white p-2 
                                     rounded-full">
                                 <IoTrash size={28}/>
                             </button>
@@ -140,12 +147,12 @@ export default function CheckList() {
 
                 <div className="flex flex-col items-center">
                 
-                    <div
+                    <button
                         onClick={() => getVideo()} 
                         className="flex shadow-sm_gray p-4 rounded-xl z-10 bg-gradient-to-r from-yellow-400 via-yellow-200 to-yellow-400"
                     >
                         <text className="font-black drop-shadow-md text-white">Fachada da Loja</text>
-                    </div>
+                    </button>
 
                     <Image alt="" src={medalha} className="w-14 h-14 -mt-2"/>
                 </div>
