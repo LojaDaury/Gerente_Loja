c27af63f4fa27cb53968a86b5d19a34b8fc65d98
unknown <lojadaury@gmail.com>
2024-05-07 14:15:20 -0400

    cam_interface

--- src/app/Dashboard/CheckList.tsx
+++ src/app/Dashboard/CheckList.tsx
@@ -21,7 +21,7 @@ export default function CheckList() {
         
         navigator.mediaDevices
         .getUserMedia({
-            video: { width: innerHeight, height: innerWidth }
+            video: { facingMode: 'environment', width: innerHeight, height: innerWidth }
         })
         .then(stream => {
             if (videoRef.current) {
